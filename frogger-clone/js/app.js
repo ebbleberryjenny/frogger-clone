935637604a00fd058bd37f6068d5b1235f6c3239
Jenny Evans <ebbleberryjenny@gmail.com>
2017-06-12 21:19:38 -0400

    Changes app.js per review comments

--- frogger-clone/js/app.js
+++ frogger-clone/js/app.js
@@ -1,116 +1,120 @@
-// Enemies our player must avoid
-var Enemy = function(x, y) {
-  // The image/sprite for our enemies, this uses
-  // a helper to easily load images
-  this.sprite = 'images/enemy-bug-1.png';
-  //sets enemy location
+'use strict';
+
+//Superclass for all characters
+var Character = function(x, y, sprite){
   this.x = x;
   this.y = y;
+  this.sprite = sprite;
+  this.speed = (Math.floor(Math.random() * 201) + 50);
 };
 
+// Subclass of character, Enemies our player must avoid
+var Enemy = function(x, y, sprite) {
+  Character.call(this, x, y, 'images/enemy-bug-1.png');
+};
+
+Enemy.prototype = Object.create(Character.prototype);
+Enemy.prototype.constructor = Enemy;
+
 // Updates the enemy's position
-// Parameter: dt, a time delta between ticks
+// Parameter: dt, a time delta between ticks &
+//rate, the factor for each enemy's speed
 Enemy.prototype.update = function(dt) {
-  // dt multiplied by any movement
-  // ensures the game runs at the same speed for
-  // all computers.
-  //resets enemy's location to beginning of screen
+  // dt multiplied by any movement ensures the game
+  // runs at the same speed for all computers.
+  //The if part of the function resets enemy's location
+  //to beginning of screen.
   if (this.x > 505){
     this.x = 5;
-  //sets speed of different enemies
+  //speeds of different enemies
   } else {
-    if (this === enemySlow){
-      this.x += 50 * dt;
-    } else if (this === enemyMedium){
-      this.x += 100 * dt;
-    } else {
-      this.x += 150 * dt;
-    }
+    this.x += this.speed * dt;
   }
-}
+};
 
 // Draw the enemy on the screen
 Enemy.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-
-//Player class
-var Player = function(x, y) {
-  this.sprite = 'images/char-cat-girl-1.png';
-  this.x = x;
-  this.y = y;
 };
+
+// Subclass of character, Player
+var Player = function(x, y, sprite) {
+  Character.call(this, x, y, 'images/char-cat-girl-1.png');
+};
+
+Player.prototype = Object.create(Character.prototype);
+Player.prototype.constructor = Player;
 
 //resets player to starting position
 Player.prototype.reset = function() {
-  player.x = 200;
-  player.y = 380;
-}
+  this.x = 200;
+  this.y = 380;
+};
 
 //runs reset function is player moves off board
 Player.prototype.offsides = function() {
-  if (player.x > 400 || player.x < 0 || player.y > 483 || player.y < -83) {
-    player.reset();
+  if (this.x > 400 || this.x < 0 || this.y > 483 || this.y < -83) {
+    this.reset();
   }
-}
+};
 
 //Checks that player is not running into any enemies with axis-aligned
 //bounding box algorithm
 Player.prototype.checkCollisions = function(enemy){
-  var rect1 = {x: enemy.x, y: enemy.y, width: 78, height: 43}
-  var rect2 = {x: this.x, y: this.y, width: 78, height: 50}
+  var rect1 = {x: enemy.x, y: enemy.y, width: 78, height: 43};
+  var rect2 = {x: this.x, y: this.y, width: 78, height: 50};
 
   if (rect1.x < rect2.x + rect2.width &&
     rect1.x + rect1.width > rect2.x &&
     rect1.y < rect2.y + rect2.height &&
     rect1.height + rect1.y > rect2.y) {
-    player.reset();
+    this.reset();
   }
-}
+};
 
 //alerts player if they win
 Player.prototype.youWin = function() {
-  if (player.y <= -8){
+  if (this.y <= -8){
     window.alert('You won!');
-    player.reset();
+    this.reset();
   }
-}
+};
 
 //updates player location by invoking checkCollisions, offsides, handleInput, &
 //youWin functions
 Player.prototype.update = function(dt) {
-  player.offsides();
-  player.checkCollisions(enemySlow);
-  player.checkCollisions(enemyMedium);
-  player.checkCollisions(enemyFast);
-  player.handleInput();
-  player.x * dt;
-  player.y * dt;
-  player.youWin();
-}
+  this.offsides();
+  this.checkCollisions(enemySlow);
+  this.checkCollisions(enemyMedium);
+  this.checkCollisions(enemyFast);
+  this.handleInput();
+  this.speed = this.x * dt;
+  this.speed = this.y * dt;
+  this.youWin();
+};
 
 //renders updated player
 Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 //function for input arrows keys
 Player.prototype.handleInput = function(key) {
   switch(key){
     case 'left':
-      player.x -= 100;
+      this.x -= 100;
     break;
     case 'up':
-      player.y -= 82;
+      this.y -= 82;
       break;
     case 'right':
-      player.x += 100;
+      this.x += 100;
       break;
     case 'down':
-      player.y += 82;
+      this.y += 82;
       break;
   }
-}
+};
 
 // Object instantiation
 // all enemy objects in an array called allEnemies
@@ -134,4 +138,4 @@ document.addEventListener('keyup', function(e) {
   };
 
   player.handleInput(allowedKeys[e.keyCode]);
-})
+});
